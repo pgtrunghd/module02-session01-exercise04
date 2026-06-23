99c0705fadae5898a8eec5cdac3baeba05062d32
Phạm Gia Trung <phamgiatrung@Mac-mini-cua-Pham.local>
2026-06-23 21:16:49 +0700

    update data type

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 void main() {
     Scanner sc = new Scanner(System.in);
     System.out.print("width = ");
-    double width = Double.parseDouble(sc.nextLine());
+    float width = Float.parseFloat(sc.nextLine());
     System.out.print("height = ");
-    double height = Double.parseDouble(sc.nextLine());
+    float height = Float.parseFloat(sc.nextLine());
     System.out.printf("Diện tích: %.2f\n", width * height);
     System.out.printf("Chu vi: %.2f", (width + height) * 2);
     sc.close();
